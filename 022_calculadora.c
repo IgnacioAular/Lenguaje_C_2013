bf65af2d8d5cc8e3038228e6f7d061efacbfd085
Ignacio Aular <ignacio_345@hotmail.com>
2013-02-10 11:18:48 -0530

    Update 022_calculadora.c

--- 022_calculadora.c
+++ 022_calculadora.c
@@ -1,22 +1,22 @@
 // Programa: calculadora.c
 // Autor: Ignacio Aular
-// Versi�n: 1.0
+// Versión: 1.0
 // Fecha: 10/02/2013
 // Hora: 10:10 a.m.
-// Descripci�n: Muestra como llamar procedimientos almacenados en una librer�a externa.
+// Descripción: Muestra como llamar procedimientos almacenados en una librería externa.
 
-// Llamado a librer�a estandar integrado
+// Llamado a librería estandar integrado
 #include <stdio.h>
-// Llamado a librer�a elaborada por el programador
+// Llamado a librería elaborada por el programador
 #include "aritmetica.h"
 
-// Declaraci�n y definici�n del m�todo principal main
+// Declaración y definición del método principal main
 int main(void)
 {
 	// Llamado al procedimiento principal
 	menu_de_opciones();
 
-	// Indica al sistema operativo que el programa termin� con �xito
+	// Indica al sistema operativo que el programa terminó con éxito
 	return 0;
 }
 
